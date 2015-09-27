d565e0088695cefca1f23e20d1cfae7b7531a20b
Minori Hiraoka <minori@minori.moe>
2015-09-27 15:08:18 +0900

    Project page and licenses web browser launch intent

--- app/src/main/java/moe/minori/pgpclipper/PGPClipperSettingsActivity.java
+++ app/src/main/java/moe/minori/pgpclipper/PGPClipperSettingsActivity.java
@@ -1,6 +1,7 @@
 package moe.minori.pgpclipper;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
@@ -25,12 +26,48 @@ public class PGPClipperSettingsActivity extends AppCompatActivity {
                 @Override
                 public boolean onPreferenceChange(Preference preference, Object newValue) {
 
-                    if ( (boolean) newValue )
+                    if ((boolean) newValue)
                         // enabled
                         getActivity().startService(new Intent(getActivity(), PGPClipperService.class));
                     else
                         // disabled
                         getActivity().stopService(new Intent(getActivity(), PGPClipperService.class));
+                    return true;
+                }
+            });
+
+            findPreference("issueTrackerPreferenceItem").setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+                @Override
+                public boolean onPreferenceClick(Preference preference) {
+
+                    Intent webBrowserLaunchIntent = new Intent(Intent.ACTION_VIEW);
+                    webBrowserLaunchIntent.setData(Uri.parse("https://github.com/Mnkai/PGPClipper"));
+                    startActivity(webBrowserLaunchIntent);
+
+                    return true;
+                }
+            });
+
+            findPreference("licensePreferenceItem").setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+                @Override
+                public boolean onPreferenceClick(Preference preference) {
+
+                    Intent webBrowserLaunchIntent = new Intent(Intent.ACTION_VIEW);
+                    webBrowserLaunchIntent.setData(Uri.parse("https://github.com/Mnkai/PGPClipper/blob/master/LICENSE"));
+                    startActivity(webBrowserLaunchIntent);
+
+                    return true;
+                }
+            });
+
+            findPreference("thirdPartyLicensePreferenceItem").setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+                @Override
+                public boolean onPreferenceClick(Preference preference) {
+
+                    Intent webBrowserLaunchIntent = new Intent(Intent.ACTION_VIEW);
+                    webBrowserLaunchIntent.setData(Uri.parse("https://github.com/Mnkai/PGPClipper/blob/master/LICENSE"));
+                    startActivity(webBrowserLaunchIntent);
+
                     return true;
                 }
             });
