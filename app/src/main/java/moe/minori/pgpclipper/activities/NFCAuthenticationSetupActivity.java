491e39956e25ff2d039738c3f7f983311ba8d5bb
Minori Hiraoka <minori@minori.moe>
2015-10-18 14:48:48 +0900

    Create NFCAuth activity

---
+++ app/src/main/java/moe/minori/pgpclipper/activities/NFCAuthenticationSetupActivity.java
@@ -0,0 +1,24 @@
+package moe.minori.pgpclipper.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Minori on 2015-10-18.
+ */
+public class NFCAuthenticationSetupActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+}
