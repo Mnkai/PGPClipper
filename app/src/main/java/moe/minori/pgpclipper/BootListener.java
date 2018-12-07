4a70ff6912f6aad805c5e1c23c8f7f0c98856037
Minori Hiraoka <minori@mnetwork.co.kr>
2018-12-07 14:19:51 +0900

    Fix issue preventing startup when device is rebooted (silly me)

--- app/src/main/java/moe/minori/pgpclipper/BootListener.java
+++ app/src/main/java/moe/minori/pgpclipper/BootListener.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
+import android.util.Log;
 
 /**
  * Created by Minori on 2015-09-26.
@@ -15,24 +16,27 @@ public class BootListener extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
+        Log.d("PGPClipperBootListener", "Received Boot Completed broadcast");
+
         // BOOT_COMPLETED” start Service
         if (ANDROID_ON.equals(intent.getAction())) {
 
             // get preference
             SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
 
-            boolean isEnabled = preferences.getBoolean("enabledCheckBox", false);
+            boolean isEnabled = preferences.getBoolean("pgpClipperEnabledCheckbox", false);
 
             Intent serviceIntent = new Intent(context, PGPClipperService.class);
 
             //Service
             if ( isEnabled )
             {
-
+                Log.d("PGPClipperBootListener", "Starting service since PGPClipper is enabled");
                 context.startService(serviceIntent);
             }
             else
             {
+                Log.d("PGPClipperBootListener", "Stopping service since PGPClipper is disabled");
                 context.stopService(serviceIntent);
             }
         }
